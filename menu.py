0e59a51da874cadf9fd6f60ce015e9a32803bfd4
Oliver Wooding <oliverwooding@icloud.com>
2022-11-13 19:13:40 +0000

    partial log functionality in GUI

--- menu.py
+++ menu.py
@@ -1,20 +1,46 @@
 """Menu Tools and Primary Executable
 
-This script forms both a collection of functions for rendering the menu of the application,
-and the primary executable script, which is to say that the program as a whole should be
-run using this script.
+This script forms both a collection of functions for
+rendering the menu of the application, and the primary
+executable script, which is to say that the program
+as a whole should be run using this script.
 
 The main dependency is tkinter, which is used for creating the GUI.
 """
 
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, Tk
+from database import get_logs
 import logging
 
 
 def clear_widget(widget: Widget):
+    """Destroys all the children of the given widget."""
     for child in widget.winfo_children():
         child.destroy()
+
+
+def update_activity_list(event: Event) -> None:
+    """Renders the appropriate lines in the
+    recent activity section."""
+    listbox: Listbox = event.widget.nametowidget(".log_frame.log_entries_list")
+    listbox.configure(foreground='black')
+    logs = get_logs()[-10:]
+    for log in logs:
+        line = f"member {log[2]} "
+        if log[0] == "OUT":
+            line += f"checked out book {log[1]}."
+        elif log[0] == "RESERVE":
+            line += f"reserved book {log[1]}."
+        elif log[0] == "RETURN":
+            line += f"returned book {log[1]}."
+        else:
+            line += f"revoked reservation on book {log[1]}."
+
+        out = StringVar(name=line)
+
+        listbox.insert(END, out)
+    return None
 
 
 def on_search_clicked(event: Event) -> None:
@@ -73,15 +99,17 @@ def render_order_view(event: Event) -> None:
 
 
 def init_menu() -> Tk:
-    """Initializes the core menu components, and then returns a reference to the window root"""
+    """Initializes the core menu components, and
+    then returns a reference to the window root"""
     # init window
-    root = Tk()
+    root: Tk = Tk()
     root.title("Library Tool: Oliver Wooding")
     root.configure(bg='black')
-    root.geometry("780x505")
-    root.bind("<<SearchClicked>>", render_search_view, "+")
-    root.bind("<<IOClicked>>", render_io_view, "+")
-    root.bind("<<OrderClicked>>", render_order_view, "+")
+    root.geometry("785x505")
+    root.bind("<<SearchClicked>>", render_search_view)
+    root.bind("<<IOClicked>>", render_io_view)
+    root.bind("<<OrderClicked>>", render_order_view)
+    root.bind("<<LogUpdate>>", update_activity_list)
 
     # init button frame
     button_frame = Frame(root, width=180, height=60, name="button_frame")
@@ -98,29 +126,32 @@ def init_menu() -> Tk:
     log_frame_label.grid(row=0, column=0, padx=20)
     log_entries = Listbox(log_frame,
                           name="log_entries_list",
-                          height=22)
+                          height=22,
+                          bg='white',
+                          listvariable=Variable(value=[], name="log_list_variable"))
     log_entries.grid(row=1, column=0, pady=5)
+    root.event_generate("<<LogUpdate>>")
 
     # init buttons
     search_button = Button(button_frame,
                            text="Search",
                            width=3,
                            height=2,
-                           font=('helvetica', 14, 'bold'))
+                           font=('helvetica', 15, 'bold'))
     search_button.grid(row=0, column=0)
     search_button.bind('<Button-1>', on_search_clicked)
     io_button = Button(button_frame,
                        text='In/Out',
                        width=3,
                        height=2,
-                       font=('helvetica', 14, 'bold'))
+                       font=('helvetica', 15, 'bold'))
     io_button.grid(row=0, column=1)
     io_button.bind('<Button-1>', on_io_clicked)
     purchase_button = Button(button_frame,
                              text='Order',
                              width=3,
                              height=2,
-                             font=('helvetica', 14, 'bold'))
+                             font=('helvetica', 15, 'bold'))
     purchase_button.grid(row=0, column=2)
     purchase_button.bind('<Button-1>', on_order_clicked)
 
@@ -133,5 +164,7 @@ def init_menu() -> Tk:
 
 
 if __name__ == "__main__":
-    logging.basicConfig(filename="DO_NOT_SUBMIT/general.log", encoding="utf-8", level=0)
+    logging.basicConfig(filename="DO_NOT_SUBMIT/general.log",
+                        encoding="utf-8",
+                        level=0)
     init_menu().mainloop()
