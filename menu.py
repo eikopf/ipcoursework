0f6cce19a8ba69bc79360312330be15c7f2ff664
Oliver Wooding <oliverwooding@icloud.com>
2022-11-13 00:03:36 +0000

    adding logging components

--- menu.py
+++ menu.py
@@ -6,6 +6,13 @@ run using this script.
 """
 
 from tkinter import *
+from tkinter import ttk
+import logging
+
+
+def clear_widget(widget: Widget):
+    for child in widget.winfo_children():
+        child.destroy()
 
 
 def on_search_clicked(event: Event) -> None:
@@ -13,10 +20,24 @@ def on_search_clicked(event: Event) -> None:
     event.widget.event_generate("<<SearchClicked>>")
 
 
+def get_search_results(event: Event) -> None:
+    # get search results into list
+    # get reference to results_list
+    # dump results into results_list
+    return None
+
+
 def render_search_view(event: Event) -> None:
-    """Renders the search window in the main viewport"""
+    """Renders the search window in the main viewport."""
+    logging.debug("switched to search view")
     viewport: Frame = event.widget.nametowidget(".viewport")
-    viewport.configure(bg="red")
+    clear_widget(viewport)
+    search_bar = ttk.Entry(viewport, width=59)
+    search_bar.grid(row=0, column=0, padx=5, pady=5)
+    search_bar.bind("<Key>", get_search_results)
+    results_list = Listbox(viewport, width=60, height=26)
+    results_list.grid(row=1, column=0, padx=5, pady=5)
+
     return None
 
 
@@ -27,8 +48,10 @@ def on_io_clicked(event: Event) -> None:
 
 
 def render_io_view(event: Event) -> None:
+    """Renders the checkout/return window in the main viewport."""
+    logging.debug("switched to io view")
     viewport: Frame = event.widget.nametowidget(".viewport")
-    viewport.configure(bg="green")
+    clear_widget(viewport)
     return None
 
 
@@ -39,8 +62,10 @@ def on_order_clicked(event: Event) -> None:
 
 
 def render_order_view(event: Event) -> None:
+    """Renders the ordering window in the main viewport."""
+    logging.debug("switched to order view")
     viewport: Frame = event.widget.nametowidget(".viewport")
-    viewport.configure(bg="blue")
+    clear_widget(viewport)
     return None
 
 
@@ -56,11 +81,11 @@ def init_menu() -> Tk:
     root.bind("<<OrderClicked>>", render_order_view, "+")
 
     # init button frame
-    button_frame = Frame(root, width=180, height=60)
+    button_frame = Frame(root, width=180, height=60, name="button_frame")
     button_frame.grid(row=0, column=0, padx=10, pady=5)
 
     # init log frame
-    log_frame = Frame(root, width=180, height=430)
+    log_frame = Frame(root, width=180, height=430, name="log_frame")
     log_frame.grid(row=1, column=0, rowspan=7, padx=10, pady=5)
     log_frame.configure(bg='purple')
 
@@ -78,10 +103,11 @@ def init_menu() -> Tk:
     # init viewport
     viewport = Frame(root, width=560, height=490, name="viewport")
     viewport.grid(row=0, column=1, columnspan=4, rowspan=8, padx=5, pady=5)
-    viewport.configure(bg='red')
+    viewport.configure(bg='darkblue')
 
     return root
 
 
 if __name__ == "__main__":
+    logging.basicConfig(filename="DO_NOT_SUBMIT/menu.log", encoding="utf-8", level=0)
     init_menu().mainloop()
